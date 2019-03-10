b9bf1defad1137ec0ab2b4c7acf2933d19724f42
hyakei <oke123543@gmail.com>
2019-03-10 11:33:34 +0000

    rolex/riva: Viper Official Bringup

--- viper_rolex.mk
+++ viper_rolex.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Its Official ViperOS
+VIPER_BUILD_TYPE := OFFICIAL
+
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
 
@@ -29,7 +32,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := viper_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
